aebf2cbae27c0653205d044e873e1ee8454644e2
Zhiyuan Zou <zouzhiyuanzju@gmail.com>
2015-11-08 21:05:34 -0600

    fixed directory path

--- Experiment.py
+++ Experiment.py
@@ -3,8 +3,8 @@ import sys
 def largest(list):
   if len(list) == 0:
     raise ValueError("Cannot call largest on empty list")
-  # max = -sys.maxint # "smallest" possible int
-  max = -sys.maxint-1
+  max = -sys.maxint # "smallest" possible int
+
   for index in range(len(list)):
     if (list[index] > max):
       max = list[index]
--- nose.sh
+++ nose.sh
@@ -1,2 +1,2 @@
-python2.7 -m nose --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=project --cover-inclusive
+python2.7 -m nose --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=/Users/zou/Documents/python/lab3 --cover-inclusive
 python -m coverage xml --include=project*
