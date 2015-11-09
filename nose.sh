fc8c493e6a87fbfdfa32ff6b8115f15f3f480be0
Zhiyuan Zou <zouzhiyuanzju@gmail.com>
2015-11-08 21:40:58 -0600

    final

--- nose.sh
+++ nose.sh
@@ -1,2 +1,2 @@
-python2.7 -m nose --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=/Users/zou/Documents/python/lab3 --cover-inclusive
-python -m coverage xml --include=project*
+python2.7 -m nose --with-xunit --all-module --traverse-namespace --with-coverage --cover-package=lab3 --cover-inclusive
+
